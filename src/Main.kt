4911e5c85e014d413f42c5a1fb7ab8675dcaac7f
GISELE RODRIGUES DOS SANTOS <25203682@dsn127m1247444.cfp127aluno.local>
2026-08-20 16:59:20 -0300

    nova aula adicionada

--- src/Main.kt
+++ src/Main.kt
@@ -66,4 +66,204 @@ fun main() {
   //val z: Double = x  //não funciona
 
     //cast: mudar o formato do dado - ex:transformar um double em int
+
+    //operações (aritmeticas):
+    val a: Int = 10
+    val b: Double = 2.5
+    val resultado = a * b //Double
+
+    val ten = 10
+    val three = 3
+
+    println(ten + three) //13   -> soma
+    println(ten - three) //7    -> subtração
+    println(ten * three) //30   -> multiplicação
+    println(ten / three) //3    -> divisão
+    println(ten % three) //1    -> porcentagem
+
+    //operações (lógicas):
+
+    /*
+    * ==  igual
+    * !=  diferente
+    * > maior
+    * < menor
+    * >= maior ou igual
+    * <= menor ou igual
+    */
+
+
+    //operações (atribuição):
+
+    /*  faz de forma resumida uma operacção maior
+    *
+    *   +=  valor atual mais novo valor
+    *   -=   valor atual menos um novo valor
+    *   *= valor atual multiplicado pelo novo valor
+    *   /=    valor atual dividido pelo novo valor
+    *   %= resto da divisão atual pelo novo valor
+    *
+    * ex: var x = 10
+    * x += 5 // x = x + 5
+    *
+    * */
+
+    //operações (logicas):
+    //&& E
+    // || ou
+    // ! not
+    nomeDaFuncao()
+    mandaSalve("Manoel")
+    mensagemParabens(idade = 28, nome = "Manoel")
+
+    fun soma (a: Int, b: Int): Int{
+        return a + b
+    }
+    //ou:
+    //fun soma(a: Int, b: Int) = a + b
+    //ou (em arrow/lamina):
+    // val soma: (Int, Int) -> Int = {a, b -> a + b}
+
+    val result = soma(10,3)
+    println(result)
+
+    exibirNota(7)
+
+    val idade2 = 18
+
+//    if (idade2 >= 18){
+//        println("Maior de idade!")
+//    }else{
+//        println("Menor de idade!")
+//    }
+
+
+    val status = if (idade2 >= 18){
+        "Maior de idade!"
+        "Teste"
+        println("Teste3")
+    }else{
+        "Menor de idade!"
+    }
+
+    println(status)
+
+    val idade3 = 20
+
+    //if/else podem ser utilizados como o "ternário" (que é if/else em uma linha)
+    //                condicao    ? valorSeTrue : valorSeFalse
+    // const result = idade >= 18 ? "Maior": "Menor"
+    println(if(idade >= 18) "Maior" else "Menor")
+
+    val dia = 3
+
+    when (dia){
+        1 -> println("Domingo")
+        2 -> println("Segunda")
+        3 -> println("Terça")
+        else -> println("Dia inválido")
+    }
+
+    val nota = 9
+    when (nota){
+        9,10 -> println("Excelente")
+        7,8 -> println("bom")
+        6 -> println("Regular")
+        else -> println("Reprovado")
+    }
+
+    //Trabalhando com when e range
+    val nota2 = 4
+
+    when {
+        nota2 >= 9 -> println("Excelente")
+        nota2 >= 6 -> println("Regular")
+        else -> println("Reprovado")
+    }
+
+    //TRABALHAR COM DADOS NULOS
+    //errado:
+    //    var nome: String = "joao"
+    //    nome = null
+
+    //CERTO:
+    //    var nome: String? = "joao"
+    //    nome =  null
+    //mas para utilizar a variavel, o compilador nos obriga a realizar uma validação antes
+    //    if(nome != null){
+    //        println(nome.length)
+    //    }
+    //OU:
+
+    //    var nome: String? = null
+    //    println(nome?.length)
+
+
+    //COLLECTIONS (Array - avô das collections) -> POUCO UTILIZADO
+    var numeros = arrayOf(1,2, 2)
+
+    println(numeros[0]) //1
+
+    numeros[1] = 50
+    println(numeros[1]) //50
+
+    //List
+    //mantem ordem de inserção dos elementos (acesso por índice)
+    //permite repetição
+    //n permite alteração (somente leitura)
+
+    var frutas = listOf("Banana","Maçã","Maçã")
+
+    println(frutas[0]) //Banana
+
+    println(frutas.size) //3
+    println(frutas.first()) //Banana
+    println(frutas.last()) //Laranja
+    println(frutas.contains("Maçã")) //true
+
+
+ //    frutas.add("Limão") //não funciona
+//    frutas.remove(2) // não funciona
+
 }
+
+//estrutura de função
+fun nomeDaFuncao (){
+    println("testando.....")
+}
+
+//função com parâmetro
+fun mandaSalve (nome: String){
+    println("Salve $nome!")
+}
+
+//função com um parâmetro pré-definido caso não venha um valor
+
+/*
+* fun mandaSalve (nome: String = "mano"){
+*   println("Salve $nome!")
+* }
+*
+* aí chama ela na main sem passar parâmetro:
+* mandaSalve()
+*
+* */
+
+fun mensagemParabens(nome: String, idade: Int){
+    println("Parabéns $nome pelos $idade anos de vida!")
+}
+
+
+fun exibirNota(nota: Int){
+    if(nota >= 9){
+        println("Excelente")
+    } else if (nota >= 6){
+        println("Bom")
+    } else {
+        println("Reprovado")
+    }
+}
+
+
+
+
